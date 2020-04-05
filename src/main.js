d48b8663092ed797cf235b3bf2f715b8aef5a7b8
Virginia Ladd <virginialadd2@gmail.com>
2020-04-05 00:41:06 -0600

    Add routing to main

--- src/main.js
+++ src/main.js
@@ -1,15 +1,9 @@
 import Library from './routes/Library.svelte';
-import Login from './routes/Login.svelte';
 import App from './App.svelte';
-
-// var app = new Library({
-// 	target: document.body,
-// 	components: { Login }
-// });
 
 var app = new App({
 	target: document.body,
-	components: { Library }
+	components: { Library, App }
 })
 
-export default login;
+export default app;
