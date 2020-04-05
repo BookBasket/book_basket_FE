354036bd4af097857f763d27dfecd150ffd349a1
Virginia Ladd <virginialadd2@gmail.com>
2020-04-05 00:03:56 -0600

    Add routing to app

---
+++ src/App.svelte
@@ -0,0 +1,18 @@
+<script>
+  import { Router, Link, Route } from "svelte-routing";
+  import Login from "./routes/Login.svelte";
+  import Library from "./routes/Library.svelte";
+
+  export let url = '';
+</script>
+
+<Router url='{url}'>
+  <nav>
+    <Link to='/'>Login</Link>
+    <Link to='library'>Library</Link>
+  </nav>
+  <div>
+    <Route path='library' component='{Library}' />
+    <Route path="/"><Login /></Route>
+  </div>
+</Router>
