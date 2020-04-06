b470db4b8825c85886fb6e901a4328d597c15958
Virginia Ladd <virginialadd2@gmail.com>
2020-04-06 11:54:55 -0600

    Removed Nav component from App routing

--- src/App.svelte
+++ src/App.svelte
@@ -3,6 +3,7 @@ import { Router, Route, Link } from 'svelte-routing';
 import Library from './routes/Library.svelte';
 import User from './routes/User.svelte';
 import Login from './routes/Login.svelte';
+import Search from './routes/Search.svelte';
 let bgImage = 'assets/basket-bg.jpg';
 let logo = 'assets/BB-Logo-White.png';
 export let url='';
@@ -14,7 +15,8 @@ export let url='';
 <Router url='{url}'>
   <div>
     <Route path='/' component='{Login}' />
-    <Route path='/library' component='{Library}' />
     <Route path='/user' component='{User}' />
+    <Route path='/library' component='{Library}' />
+    <Route path='/search' component='{Search}' />
   </div>
 </Router>
