6dc7a336d5fb988b591dee80d0ff0fb80d393d95
Virginia Ladd <virginialadd2@gmail.com>
2020-04-06 15:32:21 -0600

    Create search component with basic search input options

--- src/routes/Search.svelte
+++ src/routes/Search.svelte
@@ -8,4 +8,14 @@
 
 <section>
   <Nav />
+  <form>
+    <input type='text' value='Search...' />
+    <input type='radio' id='title' name='title' value='title' />
+    <label for='title'>Title</label>
+    <input type='radio' id='author' name='author' value='author' />
+    <label for='author'>Author</label>
+    <input type='radio' id='genre' name='genre' value='genre' />
+    <label for='Genre'>Genre</label>
+    <button type='button'>Submit</button>
+  </form>
 </section>
