dc24581b8bddd3a0878939b53f4ea891e718ee52
Virginia Ladd <virginialadd2@gmail.com>
2020-04-06 09:44:39 -0600

    Create User component for library choice

---
+++ src/routes/User.svelte
@@ -0,0 +1,18 @@
+<script>
+  import { Link } from 'svelte-routing';
+
+</script>
+
+<style>
+  button {
+    height: 300px;
+    width: 500px;
+    border-bottom: 20px;
+    color: #921C2B;
+  }
+</style>
+
+<section>
+  <Link to='/library'><button>Already Read Books</button></Link>
+  <Link to='/library'><button>Want to Read Books</button></Link>
+</section>
