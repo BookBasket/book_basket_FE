5f38e3ce7bf74d807d58074a36364ec99de78b44
Virginia Ladd <virginialadd2@gmail.com>
2020-04-08 16:11:12 -0600

    Sends a passing test through for Tavis

---
+++ src/test/Login.spec.js
@@ -0,0 +1,7 @@
+// import '@testing-library/jest-dom/extend-expect';
+// import Login from '../routes/Login';
+
+test('tests', () => {
+  // const { getByText } = render(Login)
+  // expect().toBeInTheDocument()
+})
