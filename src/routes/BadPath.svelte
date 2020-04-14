1bf3cb47edf999e56782294c782c21fc62dc6168
Virginia Ladd <virginialadd2@gmail.com>
2020-04-13 21:35:30 -0600

    Adding link for heroku

--- src/routes/BadPath.svelte
+++ src/routes/BadPath.svelte
@@ -1,8 +1,10 @@
 <script>
+import { Link } from 'svelte-routing';
+
 </script>
 
 <style>
 </style>
 
 <h1>Bad Path Page</h1>
-<button type='button'>Return Home</button>
+<Link to='/'><button type='button'>Return Home</button></Link>
